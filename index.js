8fb32c9bc19df01c49085dacae02e77b6bbc6e21
monyonko <monyonko.miriam@gmail.com>
2023-12-15 17:22:04 +0300

    Added content

---
+++ index.js
@@ -0,0 +1,16 @@
+let hamburger = document.querySelector("#hamburger");
+let nestedList = document.querySelector('.nested-list')
+nestedList.style.display = "none";
+hamburger.addEventListener('click', ()=>{
+    if (nestedList.style.display === "none"){
+        nestedList.style.display = "block"
+        hamburger.style.transform = "rotate(160deg)"  
+    }
+    else {
+        nestedList.style.display = "none";
+        hamburger.style.transform = "rotate(0deg)"
+        console.log(nestedList)
+
+    }
+})
+
